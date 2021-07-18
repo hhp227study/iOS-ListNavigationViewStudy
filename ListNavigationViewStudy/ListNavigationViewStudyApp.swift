ad8dc74143a4606de1c8d67763b42b076d506281
hhp227 <hong227@naver.com>
2021-07-18 14:46:30 +0900

    Initial commit

---
+++ ListNavigationViewStudy/ListNavigationViewStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavigationViewStudyApp.swift
+//  ListNavigationViewStudy
+//
+//  Created by 홍희표 on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct ListNavigationViewStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
